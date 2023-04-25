04b5672b1f25a443827e696759fe1535200283e1
Raf <socozy7210@gmail.com>
2023-04-25 16:34:04 +0900

    edit VoteInfoHandler

--- frame/support/src/traits/pot.rs
+++ frame/support/src/traits/pot.rs
@@ -1,11 +1,10 @@
 use super::tokens::fungibles;
+use crate::dispatch::DispatchError;
 use sp_std::vec::Vec;
 
 pub type VoteInfo<AccountId, AssetId, VoteWeight> = (AccountId, AssetId, VoteWeight);
 
 /// An interface for dealing with vote info
 pub trait VoteInfoHandler<AccountId, F: fungibles::Inspect<AccountId>> {
-	fn get_vote_info() -> Vec<VoteInfo<AccountId, F::AssetId, F::Balance>>;
-
-	fn update_vote_info();
+	fn update_vote_info(who: &AccountId, asset_id: &F::AssetId, vote_weight: F::Balance);
 }
