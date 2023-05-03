1652ffa8be507592f03516494ca58cd1fe2a1694
Raf <socozy7210@gmail.com>
2023-05-03 19:55:34 +0900

    add PotVotes & PotVote type

--- primitives/runtime/src/generic/mod.rs
+++ primitives/runtime/src/generic/mod.rs
@@ -36,5 +36,5 @@ pub use self::{
 	era::{Era, Phase},
 	header::Header,
 	unchecked_extrinsic::{SignedPayload, UncheckedExtrinsic},
-	vote::{VoteAssetId, VoteWeight, VoteAccountId, PotVote},
+	vote::{VoteAssetId, VoteWeight, VoteAccountId, PotVotes, PotVote},
 };
