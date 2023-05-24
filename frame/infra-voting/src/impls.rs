641a183b438be80d40ec20dd1b8256e90e10600a
Raf <socozy7210@gmail.com>
2023-05-24 18:16:42 +0900

    add validator set change for era

--- frame/infra-voting/src/impls.rs
+++ frame/infra-voting/src/impls.rs
@@ -1,6 +1,7 @@
+
 use crate::*;
 
-pub type MaxValidatorsOf<T> = <T as Config>::MaxValidators;
+pub type TotalNumValidatorsOf<T> = <T as Config>::TotalNumberOfValidators;
 /// Means for interacting with a specialized version of the `session` trait.
 pub trait SessionInterface<AccountId> {
 	/// Disable the validator at the given index, returns `false` if the validator was already
@@ -25,28 +26,49 @@ impl<AccountId> SessionInterface<AccountId> for () {
 }
 
 pub trait VotingHandler<T> {
-	fn update_vote_weight(session_index: SessionIndex, who: VoteAccountId, weight: VoteWeight);
+	fn update_vote_status(era_index: SessionIndex, who: VoteAccountId, weight: VoteWeight);
 }
 
 impl<T: Config> VotingHandler<T> for Pallet<T> {
-	fn update_vote_weight(session_index: SessionIndex, who: VoteAccountId, weight: VoteWeight) {
-		let vote_id: T::InfraVoteId = who.into();
+	fn update_vote_status(era_index: SessionIndex, who: VoteAccountId, weight: VoteWeight) {
+		let vote_account_id: T::InfraVoteAccountId = who.into();
 		let vote_points: T::InfraVotePoints = weight.into();
 
-		let mut vote_status = VotingStatusPerSession::<T>::get(&session_index, &vote_id);
-		vote_status.increase_weight(&vote_id, vote_points);
-		Pallet::<T>::deposit_event(Event::<T>::VotePointsAdded {
-			session_index,
-			who: vote_id,
-			points: vote_points,
-		})
+		let mut vote_status = VotingStatusPerEra::<T>::get(&era_index);
+		vote_status.add_points(&vote_account_id, vote_points);
 	}
 }
 
 impl<T: Config> VotingHandler<T> for () {
-	fn update_vote_weight(_: SessionIndex, _: VoteAccountId, _: VoteWeight) {}
+	fn update_vote_status(_: SessionIndex, _: VoteAccountId, _: VoteWeight) {}
 }
 
+// Session Pallet Rotate Order
+//
+// On Genesis:
+// `new_session_genesis()` is called
+//
+// After Genesis:
+// `on_initialize(block_number)` when session is about to end
+// `end_session(bn)` -> `start_session(bn+1)` -> `new_session(bn+2)` are called this order
+//
+// Detail
+// 1. new_session()
+// - Plan a new session and optionally returns Validator Set
+// - Potentially plan a new era
+// - Internally `trigger_new_era()` is called when planning a new era
+//
+// 2. new_session_genesis()
+// - Called only once at genesis
+// - If there is no validator set returned, session pallet's config keys are used for initial validator set
+//
+// 3. start_session()
+// - Start a session potentially starting an era
+// - Internally `start_era()` is called when starting a new era
+// 
+// 4. end_session()
+// - End a session potentially ending an era
+// - Internally `end_era()` is called when ending an era
 impl<T: Config> pallet_session::SessionManager<T::AccountId> for Pallet<T> {
 	fn new_session(new_index: SessionIndex) -> Option<Vec<T::AccountId>> {
 		log!(trace, "planning new session {}", new_index);
@@ -58,32 +80,134 @@ impl<T: Config> pallet_session::SessionManager<T::AccountId> for Pallet<T> {
 	}
 	fn start_session(start_index: SessionIndex) {
 		log!(trace, "starting session {}", start_index);
-		Self::handle_start_session(start_index);
 	}
 	fn end_session(end_index: SessionIndex) {
 		log!(trace, "ending session {}", end_index);
-		Self::handle_end_session(end_index);
 	}
 }
 
 impl<T: Config> Pallet<T> {
+	
 	fn handle_new_session(
 		session_index: SessionIndex,
 		is_genesis: bool,
-	) -> Option<BoundedVec<T::AccountId, MaxValidatorsOf<T>>> {
+	) -> Option<BoundedVec<T::AccountId, TotalNumValidatorsOf<T>>> {
 		if let Some(current_era) = CurrentEra::<T>::get() {
-			None
+			let start_session_index = StartSessionIndexPerEra::<T>::get(current_era)
+				.unwrap_or_else(|| {
+					frame_support::print("Error: start_session_index must be set for current_era");
+					0
+				});
+				let era_length = session_index.saturating_sub(start_session_index); // Must never happen.
+
+				match ForceEra::<T>::get() {
+					// Will be set to `NotForcing` again if a new era has been triggered.
+					Forcing::ForceNew => (),
+					// Short circuit to `try_trigger_new_era`.
+					Forcing::ForceAlways => (),
+					// Only go to `try_trigger_new_era` if deadline reached.
+					Forcing::NotForcing if era_length >= T::SessionsPerEra::get() => (),
+					_ => {
+						// Either `Forcing::ForceNone`,
+						// or `Forcing::NotForcing if era_length < T::SessionsPerEra::get()`.
+						return None
+					},
+				}
+
+			// New era.
+			let maybe_new_era_validators = Self::do_trigger_new_era(session_index, is_genesis);
+			if maybe_new_era_validators.is_some() &&
+				matches!(ForceEra::<T>::get(), Forcing::ForceNew)
+			{
+				Self::set_force_era(Forcing::NotForcing);
+			}
+
+			maybe_new_era_validators
 		} else {
-			// Initial era
-			None
+			log!(debug, "Starting the first era.");
+			Self::do_trigger_new_era(session_index, is_genesis)
 		}
 	}
 
-	fn handle_start_session(session_index: SessionIndex) {
+	/// Plan a new era.
+	///
+	/// * Bump the current era storage (which holds the latest planned era).
+	/// * Store start session index for the new planned era.
+	/// * Clean old era information.
+	/// * Store staking information for the new planned era
+	///
+	/// Returns the new validator set.
+	fn do_trigger_new_era(
+		session_index: SessionIndex, 
+		_is_genesis: bool
+	) -> Option<BoundedVec<T::AccountId, TotalNumValidatorsOf<T>>> {
 		
+		let new_planned_era = CurrentEra::<T>::mutate(|era| {
+			*era = Some(era.map(|old_era| old_era+1).unwrap_or(0));
+			era.unwrap()
+		});
+		StartSessionIndexPerEra::<T>::insert(&new_planned_era, session_index);
+		Some(Self::elect_validators(session_index))
+
+		// Clean old era information.
+		// Later
+		// if let Some(old_era) = new_planned_era.checked_sub(T::HistoryDepth::get() + 1) {
+		// 	Self::clear_era_information(old_era);
+		// }
 	}
 
-	fn handle_end_session(session_index: SessionIndex) {
+	/// Elect validators from `SeedTrustValidatorPool::<T>` and `PotValidatorPool::<T>`
+	/// 
+	/// First, check the number of seed trust validator.
+	/// If it is equal to number of max validators, we just elect from `SeedTrustValidatorPool::<T>`.
+	/// Otherwise, remain number of validators are elected from `PotValidatorPool::<T>`.
+	pub fn elect_validators(
+		era_index: EraIndex
+	) -> BoundedVec<T::AccountId, TotalNumValidatorsOf<T>> {
 
-	} 
+		let validators = Self::do_elect_validators(era_index);
+		validators.try_into().expect("Should be equal to total number of validators")
+	}
+
+	fn do_elect_validators(era_index: EraIndex) -> Vec<T::AccountId> {
+		let total_num_validators = T::TotalNumberOfValidators::get();
+		let num_seed_trust = NumberOfSeedTrustValidators::<T>::get();
+		let num_pot = total_num_validators - num_seed_trust;
+		let mut pot_enabled = false;
+		let mut validators: Vec<T::AccountId> = Self::do_elect_seed_trust_validators(num_seed_trust);
+		if num_pot != 0 {
+			let mut pot_validators = Self::do_elect_pot_validators(era_index, num_pot);
+			pot_enabled = true;
+			validators.append(&mut pot_validators);
+		}
+		assert!(validators.len() <= total_num_validators as usize, "Should be less or equal to total number of validators");
+		Self::deposit_event(Event::<T>::ValidatorsElected { pot_enabled });
+		validators
+	}
+
+	fn do_elect_seed_trust_validators(num_seed_trust: u32) -> Vec<T::AccountId> {
+		let seed_trust_validators = SeedTrustValidatorPool::<T>::get();
+		Self::deposit_event(Event::<T>::SeedTrustValidatorsElected);
+		seed_trust_validators
+			.iter()
+			.take(num_seed_trust as usize)
+			.cloned()
+			.collect::<Vec<_>>()
+	}
+
+	fn do_elect_pot_validators(era_index: EraIndex, num_pot: u32) -> Vec<T::AccountId> {
+		// PoT election phase 
+		let mut voting_status = VotingStatusPerEra::<T>::get(&era_index);
+		voting_status.sort_by_vote_points();
+		let pot_validators = voting_status.get_top_validators(num_pot).clone();
+		Self::deposit_event(Event::<T>::PotValidatorsElected);
+		pot_validators.try_into().expect("Should be less than total number of validators")
+	}
+
+	/// Helper to set a new `ForceEra` mode.
+	pub fn set_force_era(mode: Forcing) {
+		log!(info, "Setting force era mode {:?}.", mode);
+		ForceEra::<T>::put(mode);
+		Self::deposit_event(Event::<T>::ForceEra { mode });
+	}
 }
--- frame/infra-voting/src/lib.rs
+++ frame/infra-voting/src/lib.rs
@@ -35,10 +35,32 @@ macro_rules! log {
 		)
 	};
 }
+
+#[derive(Copy, Clone, PartialEq, Eq, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+// #[cfg_attr(feature = "std", derive(serde::Serialize, serde::Deserialize))]
+pub enum Forcing {
+	/// Not forcing anything - just let whatever happen.
+	NotForcing,
+	/// Force a new era, then reset to `NotForcing` as soon as it is done.
+	/// Note that this will force to trigger an election until a new era is triggered, if the
+	/// election failed, the next session end will trigger a new election again, until success.
+	ForceNew,
+	/// Avoid a new era indefinitely.
+	ForceNone,
+	/// Force a new era at the end of all sessions indefinitely.
+	ForceAlways,
+}
+
+impl Default for Forcing {
+	fn default() -> Self {
+		Forcing::NotForcing
+	}
+}
+
 #[derive(Encode, Decode, Clone, PartialEq, RuntimeDebug, TypeInfo)]
 #[scale_info(skip_type_params(T))]
 pub struct VotingStatus<T: Config> {
-	pub status: Vec<(T::InfraVoteId, T::InfraVotePoints)>,
+	pub status: Vec<(T::InfraVoteAccountId, T::InfraVotePoints)>,
 }
 
 impl<T: Config> Default for VotingStatus<T> {
@@ -48,15 +70,35 @@ impl<T: Config> Default for VotingStatus<T> {
 }
 
 impl<T: Config> VotingStatus<T> {
-	pub fn increase_weight(&mut self, who: &T::InfraVoteId, weight: T::InfraVotePoints) {
+	/// Add vote point for given vote account id and vote points.
+	pub fn add_points(&mut self, who: &T::InfraVoteAccountId, vote_points: T::InfraVotePoints) {
 		for s in self.status.iter_mut() {
 			if &s.0 == who {
-				s.1 = s.1.clone().saturating_add(weight.into());
+				s.1 = s.1.clone().saturating_add(vote_points.into());
 				return
 			}
 		}
-		self.status.push((who.clone(), weight.into()));
+		self.status.push((who.clone(), vote_points.into()));
 	}
+
+	/// Sort vote status for decreasing order
+	pub fn sort_by_vote_points(&mut self) {
+		self.status.sort_by(|x, y| y.1.cmp(&x.1));
+	}
+
+	/// Get top validators for given vote status.
+	/// We elect validators based on PoT which has exceeded the minimum vote points.
+	/// 
+	/// Note: 
+	/// This function should be called after `sort_by_vote_points` is called.
+	pub fn get_top_validators(&mut self, num: u32) -> Vec<T::AccountId> {
+		self.status
+			.iter()
+			.take(num as usize)
+			.filter(|vote_status| vote_status.1 >= T::MinVotePointsThreshold::get().into())
+			.map(|vote_status| vote_status.0.clone().into())
+			.collect()
+	}  
 }
 
 #[frame_support::pallet]
@@ -74,20 +116,29 @@ pub mod pallet {
 		/// The overarching event type.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
-		/// Max number of validators that can be elected, 
+		/// Total Number of validators that can be elected, 
 		/// which is composed of seed trust validators and pot validators
 		#[pallet::constant]
-		type MaxValidators: Get<u32>;
+		type TotalNumberOfValidators: Get<u32>;
+
+		/// Minimum vote points to be elected
+		#[pallet::constant]
+		type MinVotePointsThreshold: Get<u32>;
+
+		/// Number of sessions per era.
+		#[pallet::constant]
+		type SessionsPerEra: Get<SessionIndex>;
 
 		/// Simply the vote account id type for vote
-		type InfraVoteId: Parameter
+		type InfraVoteAccountId: Parameter
 			+ Member
 			+ MaybeSerializeDeserialize
 			+ sp_std::fmt::Debug
 			+ MaybeDisplay
 			+ Ord
 			+ MaxEncodedLen
-			+ From<VoteAccountId>;
+			+ From<VoteAccountId>
+			+ IsType<<Self as frame_system::Config>::AccountId>;
 
 		/// Simply the vote weight type for election
 		type InfraVotePoints: sp_runtime::traits::AtLeast32BitUnsigned
@@ -104,10 +155,6 @@ pub mod pallet {
 		/// guess.
 		type NextNewSession: EstimateNextNewSession<Self::BlockNumber>;
 
-		/// Number of sessions per era.
-		#[pallet::constant]
-		type SessionsPerEra: Get<SessionIndex>;
-
 		/// Interface for interacting with a session pallet.
 		type SessionInterface: SessionInterface<Self::AccountId>;
 	}
@@ -116,14 +163,25 @@ pub mod pallet {
 	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
 	pub enum Event<T: Config> {
 		/// Points has been added for candidate validator
-		VotePointsAdded { session_index: SessionIndex, who: T::InfraVoteId, points: T::InfraVotePoints },
-		/// Pot status has been changed
-		PotStatusChanged { status: bool },
+		VotePointsAdded { who: T::InfraVoteAccountId },
+		/// Number of seed trust validators has been changed
+		SeedTrustNumChanged,
+		/// Seed trust validator has been added to the pool
+		SeedTrustAdded { who: T::AccountId },
+		/// Validator have been elected
+		ValidatorsElected { pot_enabled: bool }, 
+		/// Seed Trust validators have been elected
+		SeedTrustValidatorsElected,
+		/// Validators have been elected by PoT
+		PotValidatorsElected,
+		/// A new force era mode was set.
+		ForceEra { mode: Forcing },
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
 		SeedTrustExceedMaxValidators,
+		NotActiveValidator,
 	}
 
 	/// The current era index.
@@ -133,15 +191,13 @@ pub mod pallet {
 	#[pallet::storage]
 	pub type CurrentEra<T> = StorageValue<_, EraIndex, OptionQuery>;
 
-	// Voting status mapped to session index and infra vote id
+	// Voting status for each era
 	#[pallet::storage]
 	#[pallet::unbounded]
-	pub type VotingStatusPerSession<T: Config> = StorageDoubleMap<
+	pub type VotingStatusPerEra<T: Config> = StorageMap<
 		_,
 		Twox64Concat,
-		SessionIndex,
-		Twox64Concat,
-		T::InfraVoteId,
+		EraIndex,
 		VotingStatus<T>,
 		ValueQuery,
 	>;
@@ -149,23 +205,27 @@ pub mod pallet {
 	// Current validators that are composed of seed trust validators and optional pot validators
 	#[pallet::storage]
 	pub type ValidatorPool<T: Config> =
-		StorageValue<_, BoundedVec<T::AccountId, T::MaxValidators>, ValueQuery>;
+		StorageValue<_, BoundedVec<T::AccountId, T::TotalNumberOfValidators>, ValueQuery>;
 
-	// Validators set to seed trust will be stored here
+	// Validators set of Seed Trust
 	#[pallet::storage]
 	#[pallet::unbounded]
 	pub type SeedTrustValidatorPool<T: Config> =
-		StorageValue<_, Vec<T::AccountId>, OptionQuery>;
+		StorageValue<_, Vec<T::AccountId>, ValueQuery>;
 
-	// Validators elected by pot will be stored here
-	#[pallet::storage]
-	#[pallet::unbounded]
-	pub type PotValidatorPool<T: Config> =
-		StorageValue<_, Vec<T::AccountId>, OptionQuery>;
-
+	/// Number of seed trust validators that can be elected
 	#[pallet::storage]
 	pub type NumberOfSeedTrustValidators<T: Config> = 
 		StorageValue<_, u32, ValueQuery>;
+	
+	/// Start Session index for era
+	#[pallet::storage]
+	pub type StartSessionIndexPerEra<T: Config> = StorageMap<_, Twox64Concat, EraIndex, SessionIndex, OptionQuery>; 
+
+	/// Mode of era forcing.
+	#[pallet::storage]
+	#[pallet::getter(fn force_era)]
+	pub type ForceEra<T> = StorageValue<_, Forcing, ValueQuery>;
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
@@ -177,12 +237,27 @@ pub mod pallet {
 			origin: OriginFor<T>,
 			num_validators: u32,
 		) -> DispatchResult {
-			// Only root can set pot status
+			// Only root can call
 			ensure_root(origin)?;
 			// Seed Trust validators number should be less than max validators
-			ensure!(num_validators <= T::MaxValidators::get(), Error::<T>::SeedTrustExceedMaxValidators);
+			ensure!(num_validators <= T::TotalNumberOfValidators::get(), Error::<T>::SeedTrustExceedMaxValidators);
 			NumberOfSeedTrustValidators::<T>::put(num_validators);
+			Self::deposit_event(Event::<T>::SeedTrustNumChanged);
+			Ok(())
+		}
 
+		#[pallet::call_index(1)]
+		#[pallet::weight(0)]
+		pub fn add_seed_trust_validator(
+			origin: OriginFor<T>,
+			who: T::AccountId,
+		) -> DispatchResult {
+			// Only root can call
+			ensure_root(origin)?;
+			let mut seed_trust_validators = SeedTrustValidatorPool::<T>::get();
+			seed_trust_validators.push(who.clone());
+			SeedTrustValidatorPool::<T>::put(seed_trust_validators);
+			Self::deposit_event(Event::<T>::SeedTrustAdded { who });
 			Ok(())
 		}
 	}
